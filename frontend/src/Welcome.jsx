f2bd31818efd59394d45a30abc3cbdcfbe3eb14c
vignesh <vigneshvk038@gmail.com>
2025-06-20 16:18:57 +0530

    seventh commit

--- frontend/src/Welcome.jsx
+++ frontend/src/Welcome.jsx
@@ -1,10 +1,9 @@
-const Welcome = (props) => {
+const Welcome = (props)=>{
     return (
         <div>
-            <h1>welcome {props.name}</h1>
-            <h1>country:{props.country}</h1>
+        <h1>Welcome {props.name}</h1>
+        <h1>Country: {props.country}</h1>
         </div>
     )
 }
-
-export default Welcome
+export default Welcome
